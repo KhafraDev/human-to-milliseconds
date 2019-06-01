a8c4bd851654915b1c1b85dd1fe1af27e1ceefb1
KhafraDev <42794878+KhafraDev@users.noreply.github.com>
2019-05-31 20:09:51 -0400

    better suited towards my use
    -removed ns, us and µs units of time
    -removed dependency of promisify-es6
    -removed callbacks altogether 
    -made some stuff easier to read

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 'use strict'
 
-const promisify = require('promisify-es6')
-
 const whole = /^((\d+(\.\d+)*)(ns|ms|us|µs|m|s|h))+$/
 const pieces = /((\d+(\.\d+)*)(ns|ms|us|µs|m|s|h))/g
 const measure = /(ns|ms|us|µs|m|s|h)/g
 
 const multipliers = {
-  ns: 1e-6,
-  us: 0.001,
-  µs: 0.001,
   ms: 1,
   s: 1000,
   m: 60000,
@@ -17,22 +12,21 @@ const multipliers = {
 }
 
 function analyse (time) {
-  let unit = time.match(measure)[0]
-  time = time.substring(0, time.length - unit.length)
+  const unit = time.match(measure)[0];
+  time = time.substring(0, time.length - unit.length);
 
-  return parseFloat(time) * multipliers[unit]
+  return parseFloat(time) * multipliers[unit];
 }
 
-module.exports = promisify(function (time, callback) {
-  if (typeof time !== 'string') {
-    return callback(new Error('the first argument must be a string'))
+module.exports = (time) => {
+  if(typeof time !== 'string') {
+      return new Error('the first argument must be a string');
   }
 
-  if (!whole.test(time)) {
-    return callback(new Error('invalid time'))
+  if(!whole.test(time)) {
+      return new Error('invalid time');
   }
 
-  callback(null, time.match(pieces).reduce((sum, currentVal) => {
-    return sum + analyse(currentVal)
-  }, 0))
-})
+  return time.match(pieces).reduce((sum, currentVal) => sum + analyse(currentVal), 0);
+}
+
