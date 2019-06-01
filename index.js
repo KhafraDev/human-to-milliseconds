fa5aee357842f66a1a10cb69b3eb79df5b9cf5a4
KhafraDev <42794878+KhafraDev@users.noreply.github.com>
2019-05-31 20:08:41 -0400

    removed stuff I don't need
    -removed ns, us and µs units of time
    -removed dependency of promisify-es6
    -removed callbacks altogether 
    -made some stuff easier to read

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const whole = /^((\d+(\.\d+)*)(ns|ms|us|µs|m|s|h))+$/
+const pieces = /((\d+(\.\d+)*)(ns|ms|us|µs|m|s|h))/g
+const measure = /(ns|ms|us|µs|m|s|h)/g
+
+const multipliers = {
+  ms: 1,
+  s: 1000,
+  m: 60000,
+  h: 3.6e+6
+}
+
+function analyse (time) {
+  const unit = time.match(measure)[0];
+  time = time.substring(0, time.length - unit.length);
+
+  return parseFloat(time) * multipliers[unit];
+}
+
+module.exports = (time) => {
+  if(typeof time !== 'string') {
+      return new Error('the first argument must be a string');
+  }
+
+  if(!whole.test(time)) {
+      return new Error('invalid time');
+  }
+
+  return time.match(pieces).reduce((sum, currentVal) => sum + analyse(currentVal), 0);
+}
